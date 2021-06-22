be323177abfdb8e4c6c4d4a0351879087ad8d19c
ashwini-bochare11 <86307213+ashwini-bochare11@users.noreply.github.com>
2021-06-22 20:50:00 +0530

    Add files via upload

---
+++ programjava.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    
+     System.out.println("Hello Sunita Sharma.");
+     System.out.println("Your age is 65+ years old.");
+     System.out.println("You will follow the following guidlines for your  better life and healthy life .");
+   
+      System.out.println("1.Take your  medicine at right time for time to time  suggested by your doctor. "); 
+       System.out.println("2.Eat a healthy diet."); 
+           System.out.println("3.Daily Exercise is very benifical and advantage to your body."); 
+            System.out.println("4.Continuously get yoga,meditation,satisfied.");
+            System.out.println("5.Roution checkup from family Doctor.");
+             System.out.println("6.Explore hobbies and skills.");
+              System.out.println("7.Continue to earn new thing.");
+               System.out.println("8.Share any problem to ure good person or friends.");
+                System.out.println("9.Early walk up in morning.");
+                 System.out.println("10.Dinner and break fast eat at right time.");
+                  System.out.println("11.Much more time play with children.");
+                   System.out.println("12.Keep smile always.");
+                    System.out.println("13. Read Book like Ramayan,Bhagawad Geeta are the best spiritual books.");
+     
+  }
+}
